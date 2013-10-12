01595c4bba2fd02df2c6b0ef63f581e057611981
Jeroen Offerijns <jeroen@offerijns.nl>
2013-10-12 13:06:22 +0200

    Added animation to mentors section

--- js/main.js
+++ js/main.js
@@ -18,6 +18,65 @@ $( document ).ready(function() {
 				);
 		});
 
+		var total_time = 20000;
+		var individual_time = 5000;
+		var animation_time = 2000;
+
+		(function animateSpeakers (i) {
+			$(".speaker").each(function(j, e){
+				window.setTimeout(function() {
+					$('img', e).animate({ 'opacity': '0.2' }, animation_time);
+					$('.hover-box', e).fadeIn(animation_time);
+
+					window.setTimeout(function() {
+						$('img', e).animate({ 'opacity': '1' }, animation_time);
+						$('.hover-box', e).fadeOut(animation_time);
+					}, individual_time);
+
+				}, Math.random() * total_time);
+			});
+			window.setTimeout(function() {
+				if (--i) animateSpeakers(i);
+			}, total_time);
+		})(1000);
+
+		// var i = 1;
+
+		// function animateSpeakers () {
+		//    setTimeout(function () {
+		// 			$('img', e).animate({ 'opacity': '0.2' }, 2000);
+		// 			$('.hover-box', e).fadeIn(2000);
+
+		// 			window.setTimeout(function() {
+		// 				$('img', e).animate({ 'opacity': '1' }, 2000);
+		// 				$('.hover-box', e).fadeOut(2000);
+		// 			}, 5000);
+
+		//       i++;
+		//       if (i < 10) {
+		//          animateSpeakers();
+		//       }
+		//    }, 3000)
+		// }
+
+
+		// (function animateSpeakers (i) {
+		// 	setTimeout(function () {
+		// 		var random = Math.round(Math.random()*10);
+		// 		var $e = $('.speaker').eq(random);
+
+		// 		$('img', $e).animate({ 'opacity': '0.2' }, 2000);
+		// 		$('.hover-box', e).fadeIn(2000);
+
+		// 		window.setTimeout(function() {
+		// 			$('img', e).animate({ 'opacity': '1' }, 2000);
+		// 			$('.hover-box', e).fadeOut(2000);
+		// 		}, 5000);
+
+		// 		if (--i) animateSpeakers(i);
+		// 	}, 500)
+		// })(1000);
+
 });
 
 
